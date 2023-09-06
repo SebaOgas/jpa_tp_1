ea033cae50e0d99aa5c1b5c8a9930b69595098b2
SebaOgas <143031800+SebaOgas@users.noreply.github.com>
2023-09-06 19:24:49 -0300

    Archivos de inicialización de SpringBoot añadidos
    Generados por Spring Inicializr

---
+++ jpatp1/src/main/java/com/utn/jpatp1/Jpatp1Application.java
@@ -0,0 +1,13 @@
+package com.utn.jpatp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jpatp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jpatp1Application.class, args);
+	}
+
+}
---
+++ jpatp1/src/test/java/com/utn/jpatp1/Jpatp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utn.jpatp1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jpatp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
